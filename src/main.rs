b09d45dbe761812ff2567f676d9c26c26230271b
AU68570 <maxim.guerard.1@ens.etsmtl.ca>
2025-01-23 14:38:53 -0500

    First test to read line from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+// use clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Bytes};
+
+const CODE_VERSION:u8 = 1;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+
+
+    println!("COMETS database Creator!");
+    let file = File::open("COMETS01.txt")?;
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+
+
+    let mut byte_read = reader.read_line(&mut line)?;
+
+    assert_eq!("COMETS_DATABASE_FILE", line.trim());
+    line.clear();
+    byte_read = reader.read_line(&mut line)?;
+    assert_eq!(format!("Version: {CODE_VERSION}"), line.trim());
+    line.clear();
+    byte_read = reader.read_line(&mut line)?;
+    println!("{}", line.trim());
+    line.clear();
+    byte_read = reader.read_line(&mut line)?;
+    println!("{}", line.trim());
+    line.clear();
+    byte_read = reader.read_line(&mut line)?;
+    line.clear();
+    Ok(())
+
 }
+
+
--- target/debug/COMETS_DB_CLI.d
+++ target/debug/COMETS_DB_CLI.d
@@ -1,1 +1,1 @@
-/home/dirusy/Documents/universiter/COMETS/COMETS_DB_CLI/target/debug/COMETS_DB_CLI: /home/dirusy/Documents/universiter/COMETS/COMETS_DB_CLI/src/main.rs
+/home/dirusy/Documents/universiter/COMETS/COMETS/target/debug/COMETS_DB_CLI: /home/dirusy/Documents/universiter/COMETS/COMETS/src/main.rs
